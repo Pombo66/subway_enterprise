aa00411d69e4027b0882608c2e895195610997c1
Khalid Gehlan <khalidgehlan@hotmail.com>
2025-11-12 13:52:00 +0000

    Reset migrations for PostgreSQL

--- packages/db/prisma/migrations/20251006161354_add_modifier_system/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Store" ADD COLUMN "city" TEXT;
-
--- CreateIndex
-CREATE INDEX "Store_city_idx" ON "Store"("city");
--- packages/db/prisma/migrations/20251031140545_add_address_and_postcode/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Store" ADD COLUMN "address" TEXT;
-ALTER TABLE "Store" ADD COLUMN "postcode" TEXT;
-
--- CreateIndex
-CREATE INDEX "Store_postcode_idx" ON "Store"("postcode");
